159c09131a09ded60d6e6cdb2c5f2c5f50622b33
Fersol <alex-2011.s@yandex.ru>
2020-04-12 13:26:24 +0300

    refactoring

--- plot.py
+++ plot.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
 
-filename = "cwnd.tr"
-df = pd.read_csv(filename, sep=" ", header=None)
-plt.plot(df[0], df[1])
-plt.savefig('cwnd.png')
+def parse_args():
+    parser = argparse.ArgumentParser(add_help=True, description="Files to plot")
+    parser.add_argument("-files", "--files", nargs="+", required=False,
+                        default=('cwndVegas.tr', 'cwndNewReno.tr', 'cwndBic.tr'),
+                        help="List of files to plot")
+    args=parser.parse_args()
+    return vars(args)
+
+if __name__ == "__main__":
+    args = parse_args()
+    for filename in args['files']:
+        print(f'Start plotting {filename}')
+        filesave = filename.split('.')[0] + '.png'
+        df = pd.read_csv(filename, sep=" ", header=None)
+        plt.figure()
+        plt.plot(df[0], df[1])
+        plt.savefig(filesave)
+        print(f'End plotting')
 
 
--- tcp-congestion.cc
+++ tcp-congestion.cc
@@ -16,13 +16,11 @@
  
  // Network topology
  //
- //       n0 ----------- n1
- //            500 Kbps
- //             5 ms
+ //       n0 ----------- r1 ------------ n2
+ //            100 Mbps        50 Mbps
+ //             10 ms          10 ms
  //
- // - Flow from n0 to n1 using BulkSendApplication.
- // - Tracing of queues and packet receptions to file "tcp-bulk-send.tr"
- //   and pcap tracing available when tracing is turned on.
+ // - Flow from n0 to n2 using BulkSendApplication.
  
  #include <string>
  #include <fstream>
@@ -34,93 +32,83 @@
  #include "ns3/packet-sink.h"
  #include "ns3/object.h"
  #include "ns3/traced-value.h"
-  #include "ns3/tcp-header.h"
-   #include "ns3/udp-header.h"
-   #include "ns3/enum.h"
-   #include "ns3/event-id.h"
-   #include "ns3/flow-monitor-helper.h"
-   #include "ns3/ipv4-global-routing-helper.h"
+ #include "ns3/tcp-header.h"
+ #include "ns3/udp-header.h"
+ #include "ns3/enum.h"
+ #include "ns3/event-id.h"
+ #include "ns3/flow-monitor-helper.h"
+ #include "ns3/ipv4-global-routing-helper.h"
  #include "ns3/trace-source-accessor.h"
-#include "ns3/traffic-control-helper.h"
+ #include "ns3/traffic-control-helper.h"
  #include <iostream>
  
  using namespace ns3;
  
  NS_LOG_COMPONENT_DEFINE ("TcpBulkSendExample");
-
+ 
+ // Variables for tracing
  static Ptr<OutputStreamWrapper> cWndStream;
  static uint32_t cWndValue;
  static bool firstCwnd = true;
  
-static void 
- CwndTracer (uint32_t oldval, uint32_t newval)
- {
-   //std::cout << Simulator::Now().GetSeconds() <<" Moving cwnd from " << oldval << " to " << newval << '\n';
-   if (firstCwnd)
-     {
-       *cWndStream->GetStream () << "0.0 " << oldval << std::endl;
-       firstCwnd = false;
-     }
+ static void CwndTracer (uint32_t oldval, uint32_t newval){
+   if (firstCwnd){
+     *cWndStream->GetStream () << "0.0 " << oldval << std::endl;
+     firstCwnd = false;
+   }
    *cWndStream->GetStream () << Simulator::Now ().GetSeconds () << " " << newval << std::endl;
    cWndValue = newval;
  }
 
-static void
-TraceCwnd(std::string name){
-   std::cout << name << "\n";
+static void TraceCwnd(std::string name){
+   std::cout << "trace: " << name << "\n";
    AsciiTraceHelper ascii;
    cWndStream = ascii.CreateFileStream (name.c_str ());
    Config::ConnectWithoutContext ("/NodeList/1/$ns3::TcpL4Protocol/SocketList/0/CongestionWindow", MakeCallback (&CwndTracer));
 }
 
- int
- main (int argc, char *argv[])
- {
- 
-   bool tracing = false;
+ int main (int argc, char *argv[]){
+   // Default values for command line arguments
    uint32_t maxBytes = 0;
+   std::string congestionAlg = "NewReno"; 
+   uint32_t duration = 120;
  
- //
- // Allow the user to override any of the defaults at
- // run-time, via command-line arguments
- //
    CommandLine cmd;
-   cmd.AddValue ("tracing", "Flag to enable/disable tracing", tracing);
-   cmd.AddValue ("maxBytes",
-                 "Total number of bytes for application to send", maxBytes);
+   // Allow the user to override any of the defaults at
+   // run-time, via command-line arguments
+   cmd.AddValue ("maxBytes", "Total number of bytes for application to send", maxBytes);
+   cmd.AddValue ("duration", "Duration of the experiment in seconds", duration);
+   cmd.AddValue ("congestionAlg", "Congestion algorithm: NewReno, Vegas, Bic", congestionAlg);
    cmd.Parse (argc, argv);
-
-   //Config::SetDefault ("ns3::TcpL4Protocol::SocketType", StringValue ("ns3::TcpNewReno"));
-   Config::SetDefault ("ns3::TcpL4Protocol::SocketType", StringValue ("ns3::TcpVegas"));
-   //Config::SetDefault ("ns3::TcpL4Protocol::SocketType", StringValue ("ns3::TcpBic"));
-   //Config::SetDefault("ns3::TcpSocketBase::MaxWindowSize", UintegerValue (65536));
-   Config::SetDefault("ns3::TcpSocketBase::Sack", BooleanValue (false));
-   Config::SetDefault("ns3::TcpSocketBase::Timestamp", BooleanValue (false));
-   Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue (1460));
-   Config::SetDefault("ns3::TcpSocket::SndBufSize", UintegerValue (13107200));
-   Config::SetDefault("ns3::TcpSocket::RcvBufSize", UintegerValue (13107200));
-   Config::SetDefault("ns3::TcpSocket::InitialCwnd", UintegerValue (1));
-   Config::SetDefault("ns3::TcpSocket::InitialSlowStartThreshold", UintegerValue (1));
    
- //
- // Explicitly create the nodes required by the topology (shown above).
- //
+   // Congestion algorithm in ns3
+   std::string congestionAlgName = "ns3::Tcp" + congestionAlg;
+   // Filename for saving trace
+   std::string traceName = "cwnd" + congestionAlg + ".tr";
+
+   // Set TCP configuration
+   Config::SetDefault ("ns3::TcpL4Protocol::SocketType", StringValue (congestionAlgName));
+   Config::SetDefault ("ns3::TcpSocketBase::Sack", BooleanValue (false));
+   Config::SetDefault ("ns3::TcpSocketBase::Timestamp", BooleanValue (false));
+   Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (1460));
+   Config::SetDefault ("ns3::TcpSocket::SndBufSize", UintegerValue (13107200));
+   Config::SetDefault ("ns3::TcpSocket::RcvBufSize", UintegerValue (13107200));
+   Config::SetDefault ("ns3::TcpSocket::InitialCwnd", UintegerValue (1));
+   Config::SetDefault ("ns3::TcpSocket::InitialSlowStartThreshold", UintegerValue (1));
+   
+
+   
    NS_LOG_INFO ("Create nodes.");
+   // Explicitly create the nodes required by the topology (shown above).
    NodeContainer gateway;
    gateway.Create (1);
    NodeContainer sources;
    sources.Create (1);
    NodeContainer sinks;
-   sinks.Create (1);
-
-   TrafficControlHelper tcHelper;
- 
+   sinks.Create (1); 
  
    NS_LOG_INFO ("Create channels.");
- 
- //
- // Explicitly create the point-to-point link required by the topology (shown above).
- //
+   // Explicitly create the point-to-point link required by the topology (shown above).
    PointToPointHelper sourceToGateway;
    sourceToGateway.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
    sourceToGateway.SetChannelAttribute ("Delay", StringValue ("10ms"));
@@ -132,82 +120,58 @@ TraceCwnd(std::string name){
    gatewayToSink.SetChannelAttribute ("Delay", StringValue ("10ms"));
    gatewayToSink.SetQueue ("ns3::DropTailQueue", "MaxSize", StringValue ("50p"));
    sourceToGateway.SetDeviceAttribute("InterframeGap", StringValue("0ms"));
- 
- //
- // Install the internet stack on the nodes
- //
+
+   // Install the internet stack on the nodes
    InternetStackHelper internet;
    internet.InstallAll ();
  
- //
- // We've got the "hardware" in place.  Now we need to add IP addresses.
- //
+   // We've got the "hardware" in place.  Now we need to add IP addresses.
    NS_LOG_INFO ("Assign IP Addresses.");
    Ipv4AddressHelper ipv4;
    ipv4.SetBase ("10.1.1.0", "255.255.255.0");
 
-   Ipv4InterfaceContainer sink_interfaces;
-   
+   // Install channels between current devices 
    NetDeviceContainer devs;
    devs =  sourceToGateway.Install(sources.Get(0), gateway.Get(0));
-   //std::cout << sourceToGateway.GetDeviceAttribute("InterframeGap").GetSeconds() << "\n";
-   
    ipv4.NewNetwork();
    Ipv4InterfaceContainer interfaces = ipv4.Assign(devs);
+
    devs = gatewayToSink.Install(gateway.Get(0), sinks.Get(0));
-   
    ipv4.NewNetwork();
    interfaces = ipv4.Assign(devs);
+
+   Ipv4InterfaceContainer sink_interfaces;
    sink_interfaces.Add(interfaces.Get(1));
         
+   // Enable routing
    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
    
    NS_LOG_INFO ("Create Applications.");
- 
- //
- // Create a BulkSendApplication and install it on node 0
- //
-   uint16_t port = 9;  // well-known echo port number
- 
+   uint16_t port = 9; 
+   
+   // Create a SinklApplication and install it on node 2
    Address sinkLocalAddress (InetSocketAddress(Ipv4Address::GetAny(), port));  
-
-   PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", sinkLocalAddress);
-   //sinkHelper.SetAttribute("Protocol", TypeIdValue(TcpSocketFactory::GetTypeId ())); 
+   PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", sinkLocalAddress); 
    ApplicationContainer sinkApps = sinkHelper.Install (sinks);
    sinkApps.Start (Seconds (0.0));
-   sinkApps.Stop (Seconds (120.0));
+   sinkApps.Stop (Seconds (duration));
 
-
-
+   // Create a BulkSendApplication and install it on node 0
    AddressValue remoteAddress (InetSocketAddress(sink_interfaces.GetAddress(0,0), port));
    BulkSendHelper source ("ns3::TcpSocketFactory", Address());
-   source.SetAttribute("Remote", remoteAddress);
+   source.SetAttribute ("Remote", remoteAddress);
    // Set the amount of data to send in bytes.  Zero is unlimited.
    source.SetAttribute ("MaxBytes", UintegerValue (maxBytes));
    ApplicationContainer sourceApp = source.Install (sources.Get (0));
    sourceApp.Start (Seconds (0.0));
-   sourceApp.Stop (Seconds (120.0));
+   sourceApp.Stop (Seconds (duration));
         
-   Config::ConnectWithoutContext ("/NodeList/1/$ns3::TcpL4Protocol/SocketList/0/CongestionWindow", MakeCallback (&CwndTracer));
-   
-   Simulator::Schedule(Seconds(0.001), &TraceCwnd, "cwnd.tr");
+   // Enable tracing for cwnd change
+   Simulator::Schedule(Seconds(0.001), &TraceCwnd, traceName);
 
- // Set up tracing if enabled
- //
- //  if (tracing)
-  //   {
-   //    AsciiTraceHelper ascii;
-   //    pointToPoint.EnableAsciiAll (ascii.CreateFileStream ("tcp-bulk-send.tr"));
-   //    pointToPoint.EnablePcapAll ("tcp-bulk-send", false);
-  //   }
- 
- //
- // Now, do the actual simulation.
- //
    NS_LOG_INFO ("Run Simulation.");
-   Simulator::Stop (Seconds (120.0));
+   Simulator::Stop (Seconds (duration));
    Simulator::Run ();
-
    Simulator::Destroy ();
    NS_LOG_INFO ("Done.");
  
